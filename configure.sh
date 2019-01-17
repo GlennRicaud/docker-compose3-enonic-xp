0f0ba8fb4e7f3a6f358a0cae23b2a29f4c54b1bd
runarmyklebust <rmy@enonic.com>
2019-01-17 10:58:06 +0100

    added passwd generating

--- configure.sh
+++ configure.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
+xpProj="exp"
+apacheProj="apache2"
+
 
 echo "###############################################################################"
 echo "### Enonic XP configurator"
-INSTANCE_HOSTNAME=$1
+instanceHostname=$1
 
-if [[ "x$1" = "x" ]]
-	then
+if [ -z "$instanceHostname" ]; then
 	echo "hostname argument is missing, aborting."
 	exit 1
 fi
 
-APACHE2_VHOST_TEMPLATE=apache2/sites/vhost.example.conf.template
-EXP_VHOST_FILE=exp/config/com.enonic.xp.web.vhost.cfg
-
-
-echo "###############################################################################"
-echo "### Creating apache2 config for $INSTANCE_HOSTNAME"
-
-cp $APACHE2_VHOST_TEMPLATE apache2/sites/$INSTANCE_HOSTNAME.conf
-sed -i".tmp" -e "s/SITE_HOSTNAME_ESCAPED/$(echo $INSTANCE_HOSTNAME | sed 's/\./\\\\./g')/g" apache2/sites/$INSTANCE_HOSTNAME.conf
-sed -i".tmp" -e "s/SITE_HOSTNAME/$INSTANCE_HOSTNAME/g" apache2/sites/$INSTANCE_HOSTNAME.conf
-rm apache2/sites/$INSTANCE_HOSTNAME.conf.tmp
+apache2VHostTemplate=apache2/sites/vhost.example.conf.template
+expVHostFile=exp/config/com.enonic.xp.web.vhost.cfg
 
 echo "###############################################################################"
-echo "### Adding $INSTANCE_HOSTNAME to Enonic XP vhosts"
+echo "### Creating apache2 config for $instanceHostname"
 
-sed -i".tmp" -e "s/SITE_HOSTNAME/$INSTANCE_HOSTNAME/g" $EXP_VHOST_FILE
-rm $EXP_VHOST_FILE.tmp
+cp $apache2VHostTemplate apache2/sites/$instanceHostname.conf
+sed -i".tmp" -e "s/SITE_HOSTNAME_ESCAPED/$(echo $instanceHostname | sed 's/\./\\\\./g')/g" apache2/sites/$instanceHostname.conf
+sed -i".tmp" -e "s/SITE_HOSTNAME/$instanceHostname/g" apache2/sites/$instanceHostname.conf
+rm apache2/sites/$instanceHostname.conf.tmp
 
 echo "###############################################################################"
-echo "### Adding $INSTANCE_HOSTNAME to docker-compose.yml"
+echo "### Adding $instanceHostname to Enonic XP vhosts"
 
-sed -i".tmp" -e "s/SITE_HOSTNAME/$INSTANCE_HOSTNAME/g" docker-compose.yml
+sed -i".tmp" -e "s/SITE_HOSTNAME/$instanceHostname/g" $expVHostFile
+rm $expVHostFile.tmp
+
+echo "###############################################################################"
+echo "### Adding $instanceHostname to docker-compose.yml"
+
+sed -i".tmp" -e "s/SITE_HOSTNAME/$instanceHostname/g" docker-compose.yml
 rm docker-compose.yml.tmp
+
+
+echo "###############################################################################"
+echo "### Generate and store password"
+echo "###############################################################################"
+
+suPasswdClear=$(openssl rand -base64 16 | tr -cd '[[:alnum:]]')
+hashedPwd=$(echo -n ${suPasswdClear} | shasum -a 512 | awk '{print $1}')
+suPasswd=$(echo "{sha512}${hashedPwd}")
+
+sed -i".tmp" -e "s/xp.suPassword=.*/$(echo xp.suPassword=$suPasswdClear)/g" $xpProj/env.sh
+sed -i".tmp" -e "s/xp.suPassword=.*/$(echo xp.suPassword=$suPasswd)/g" $xpProj/config/system.properties
+rm $xpProj/env.sh.tmp
+rm $xpProj/config/system.properties.tmp
 
 echo "###############################################################################"
 echo "### Ready to build and deploy with docker-compose"
