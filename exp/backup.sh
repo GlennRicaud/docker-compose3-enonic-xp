156c194e1bb8e5d81b9ba50d453a1e41dd560d15
runarmyklebust <rmy@enonic.com>
2019-09-02 15:00:02 +0200

    fixed path for snapshot check in xp7

--- exp/backup.sh
+++ exp/backup.sh
@@ -2,4 +2,4 @@
 
 # Returns exit code 0 if there are a snapshot thats newer than 1440 minutes ( 24h ).
 echo "Found the following snapshots:"
-eval find /enonic-xp/home/snapshots/  -maxdepth 1 -cmin -1440 -iname 'snapshot*' -type f | egrep '.*'
+eval find /enonic-xp/home/snapshots/  -maxdepth 1 -cmin -1440 -iname 'snap*' -type f | egrep '.*'
